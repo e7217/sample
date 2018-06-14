b0bfb926f84cc64b4743722b39532fe113e68bb9
e7217 <e7217@naver.com>
2018-06-14 17:34:36 +0900

    180612

--- setting.py
+++ setting.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 cmd1 = "sudo sed -i 's/www.google.com/172.16.0.1/g' ./raw_2.py"
 cmd2 = 'sudo python -m py_ocmpile raw_2.py'
@@ -6,6 +7,9 @@ cmd3 = 'sudo mv raw_2.pyc ../raw_.pyc'
 cmd4 = 'cd ..'
 
 os.system(cmd1)
+time.sleep(1)
 os.system(cmd2)
+time.sleep(1)
 os.system(cmd3)
+time.sleep(1)
 os.system(cmd4)
